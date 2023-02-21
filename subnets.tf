5a9665f8de9bc7a5acc58bd3369378e794201552
lolitaad989 <lolitaad19@gmail.com>
2023-02-21 11:03:24 +0530

    tf basics

--- subnets.tf
+++ subnets.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public-subnet" {
   count = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
+  availability_zone = element(var.AZ, count.index)
 
   tags = {
     Name = "robot-public-subnet-${element(var.AZ, count.index)}"
