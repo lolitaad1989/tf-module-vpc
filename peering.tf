f004daba00ea0d3bab79bed6433a6547338428b6
lolitaad989 <lolitaad19@gmail.com>
2023-02-21 21:08:40 +0530

    tf push

--- peering.tf
+++ peering.tf
@@ -9,14 +9,4 @@ resource "aws_vpc_peering_connection" "peering" {
     Name = "robot-${var.ENV}-default-vpc-peering"
   }
 }
-# Creates peering between robot vpc to Default VPC
 
-resource "aws_vpc_peering_connection" "peering" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.DEFAULT_VPC_ID
-  auto_accept   = true                  # This options is valid if bpth the VPC's are in the same account
-
-  tags = {
-    Name = "robot-${var.ENV}-default-vpc-peering"
-  }
-}
