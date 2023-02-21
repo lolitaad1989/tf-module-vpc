3aae48e0a108e167204d996224247dd5989ef62a
lolitaad989 <lolitaad19@gmail.com>
2023-02-21 11:05:19 +0530

    tf basics

--- subnets.tf
+++ subnets.tf
@@ -8,3 +8,14 @@ resource "aws_subnet" "public-subnet" {
     Name = "robot-public-subnet-${element(var.AZ, count.index)}"
   }
 }
+
+resource "aws_subnet" "private-subnet" {
+  count = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
+  availability_zone = element(var.AZ, count.index)
+
+  tags = {
+    Name = "robot-private-subnet-${element(var.AZ, count.index)}"
+  }
+}
